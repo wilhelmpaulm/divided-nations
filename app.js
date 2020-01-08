af3178c0cb44e6dee4eb1579be8e0785d047570f
wilhelm martinez <wilhelmpaulm@gmail.com>
2020-01-09 00:11:04 +0800

    update jshintrc and mongodb credentials

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var Message = mongoose.model("Message", {
   message: String
 });
 
-var dbUrl = "mongodb://username:password@ds257981.mlab.com:57981/simple-chat";
+var dbUrl = "mongodb://127.0.0.1:27017/divided-nations";
 
 app.get("/messages", (req, res) => {
   Message.find({}, (err, messages) => {
