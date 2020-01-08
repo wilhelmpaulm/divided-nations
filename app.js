3e5aea2a4a42d0460e6a2551381f53fefa8f2a2f
wilhelm martinez <wilhelmpaulm@gmail.com>
2020-01-08 23:47:48 +0800

    created Dockerfile for easier deployment and installed required packages

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
-var express = require('express');
-var bodyParser = require('body-parser')
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var mongoose = require("mongoose");
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var Message = mongoose.model('Message', {
-    name: String,
-    message: String
-})
+var Message = mongoose.model("Message", {
+  name: String,
+  message: String
+});
 
-var dbUrl = 'mongodb://username:password@ds257981.mlab.com:57981/simple-chat'
+var dbUrl = "mongodb://username:password@ds257981.mlab.com:57981/simple-chat";
 
-app.get('/messages', (req, res) => {
-    Message.find({}, (err, messages) => {
-        res.send(messages);
-    })
-})
+app.get("/messages", (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
 
-app.get('/messages', (req, res) => {
-    Message.find({}, (err, messages) => {
-        res.send(messages);
-    })
-})
+app.get("/messages", (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
 
-app.post('/messages', (req, res) => {
-    var message = new Message(req.body);
-    message.save((err) => {
-        if (err)
-            sendStatus(500);
-        io.emit('message', req.body);
-        res.sendStatus(200);
-    })
-})
+app.post("/messages", (req, res) => {
+  var message = new Message(req.body);
+  message.save(err => {
+    if (err) sendStatus(500);
+    io.emit("message", req.body);
+    res.sendStatus(200);
+  });
+});
 
-io.on('connection', () => {
-    console.log('a user is connected')
-})
+io.on("connection", () => {
+  console.log("a user is connected");
+});
 
-mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
-    console.log('mongodb connected', err);
-})
+mongoose.connect(dbUrl, { useMongoClient: true }, err => {
+  console.log("mongodb connected", err);
+});
 
 var server = http.listen(3001, () => {
-    console.log('server is running on port', server.address().port);
+  console.log("server is running on port", server.address().port);
 });
