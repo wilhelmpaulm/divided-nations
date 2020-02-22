a885f13c50d244046f88f4f801ff430c493ebc4b
wilhelmpaulm <wilhelmpaulm@gmail.com>
2020-02-22 17:27:51 +0800

    command execution chaining

--- mvp/tester.js
+++ mvp/tester.js
@@ -1,13 +1,26 @@
+const EventEmitter = require('events')
+const emitter = new EventEmitter();
+
 class Command {
   constructor () {
     this.execution = undefined;
+    this.previous = undefined;
   }
+
+  setPrevious (command) {
+    this.previous = command;
+  }
+
   execute () {
     this.execution = setTimeout(
-      () => { console.log('command executed') },
+      () => {
+        console.log('command executed');
+        if (this.previous) this.previous.execute();
+      },
       5000
     );
   }
+
   cancel () {
     if (this.execution) clearInterval(this.execution);
   }
@@ -19,7 +32,10 @@ class CommandStack {
   }
 
   addCommand (com) {
+    this.pauseExecutions();
+    if (this.stack.length) com.setPrevious(this.stack[this.stack.length - 1]);
     this.stack.push(com);
+    this.startLastExecution();
   }
 
   pauseExecutions () {
@@ -33,5 +49,14 @@ class CommandStack {
   }
 }
 
+emitter.on('ADD_CARD', (data) => {
+  data.commandStack.pauseExecutions();
+  data.commandStack.startLastExecution();
+});
+
+
 const commandStack = new CommandStack();
-commandStack.add
+commandStack.addCommand(new Command());
+commandStack.addCommand(new Command());
+commandStack.addCommand(new Command());
+console.log(commandStack.stack.length)
